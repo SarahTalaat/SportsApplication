7224bccd6968352266f309571f9f0b3be49bde43
ahmedrefat21 <ahmedref21@gmail.com>
2024-05-20 22:35:04 +0300

    solve packages conflict

---
+++ SportsApplication/Module/Favourites/test3.swift
@@ -0,0 +1,8 @@
+//
+//  test3.swift
+//  SportsApplication
+//
+//  Created by Ahmed Refat on 20/05/2024.
+//
+
+import Foundation
---
+++ SportsApplication/Module/Leagues/test1.swift
@@ -0,0 +1,8 @@
+//
+//  test1.swift
+//  SportsApplication
+//
+//  Created by Ahmed Refat on 20/05/2024.
+//
+
+import Foundation
