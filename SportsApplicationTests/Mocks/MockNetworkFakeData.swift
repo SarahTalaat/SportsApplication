8e6203e6e34161d192528b59af4d514e887ab253
SarahTalaat <sarahtalaatammar.work@gmail.com>
2024-05-28 07:26:33 +0200

    TeamDetails is tested successfully

--- SportsApplicationTests/Mocks/MockNetworkFakeData.swift
+++ SportsApplicationTests/Mocks/MockNetworkFakeData.swift
@@ -14,22 +14,24 @@ class MockNetworkFakeData {
     static let coach = Coach(coach_name: "name")
     static let coaches = Coaches(coach_name: "Coach Smith")
     static let players = Players(player_image: "player_image_url", player_name: "John Doe")
-
-    static let fakeTeamDetails: ResultTeamDetails = {
-        return ResultTeamDetails(team_key: 1, team_name: "name", team_logo: "logo", players: [players], coaches: [coaches])
-    }()
-        
-    static let fakeTeam: Team = {
-        return Team(team_key: 1, team_name: "name", team_logo: "logo", players: [player], coaches: [coach])
-    }()
-        
     static let fakeSport: Sport = Sport(name: "Sarah", image: "image")
+    
+    
+    static let fakeTeamDetails: ResultTeamDetails = ResultTeamDetails(team_key: 1, team_name: "name", team_logo: "logo", players: [players], coaches: [coaches])
+
+    static let fakeTeam: Team = Team(team_key: 1, team_name: "name", team_logo: "logo", players: [player], coaches: [coach])
 
     static let fakeEventUpcoming: Event = Event(eventKey: 1, eventDate: "date", eventTime: "time", eventHomeTeam: "hometeam", homeTeamKey: 1, eventAwayTeam: "awayTeam", awayTeamKey: 1, homeTeamLogo: "homeLogo", awayTeamLogo: "awayLogo", leagueRound: "leagueRound", eventStadium: "eventStadium", finalResult: "finalResult", eventStatus: "eventStatus")
+    
+    static let fakeEventLatest: Event = Event(eventKey: 1, eventDate: "date", eventTime: "time", eventHomeTeam: "hometeam", homeTeamKey: 1, eventAwayTeam: "awayTeam", awayTeamKey: 1, homeTeamLogo: "homeLogo", awayTeamLogo: "awayLogo", leagueRound: "leagueRound", eventStadium: "eventStadium", finalResult: "finalResult", eventStatus: "eventStatus")
     
     static let fakeLeague = League(league_key: 1, league_name: "Ahly", country_key: 1, country_name: "Egypt", league_logo: "logo", country_logo: "logo")
     
     static let fakeLeagueResponse = MyResponse(success: 1, result: [fakeLeague])
+    static let fakeTeamDetailsResponse = MyResponse(success: 1, result: [fakeTeamDetails])
+    static let fakeAllTeamsResponse = MyResponse(success: 1, result: [fakeTeam])
+    static let fakeUpComingEventResponse = MyResponse(success: 1, result: [fakeEventUpcoming])
+    static let fakeLatestEventResponse = MyResponse(success: 1, result: [fakeEventLatest])
     
     enum ResponseWithError: Error {
         case responseError
@@ -42,12 +44,52 @@ class MockNetworkFakeData {
     }
 }
 
-    extension MockNetworkFakeData {
-        func fetchLeagueDataFromAPI(url: String, param: Parameters, completionHandler: @escaping (MyResponse<League>?) -> Void) {
-            if shouldReturnError {
-                completionHandler(nil)
-            } else {
-                completionHandler(MockNetworkFakeData.fakeLeagueResponse)
-            }
+extension MockNetworkFakeData {
+    func fetchLeagueDataFromAPI(url: String, param: Parameters, completionHandler: @escaping (MyResponse<League>?) -> Void) {
+        if shouldReturnError {
+            completionHandler(nil)
+        } else {
+            completionHandler(MockNetworkFakeData.fakeLeagueResponse)
         }
     }
+}
+
+extension MockNetworkFakeData {
+    func fetchTeamDetailsDataFromAPI(url: String, param: Parameters, completionHandler: @escaping (MyResponse<ResultTeamDetails>?) -> Void) {
+        if shouldReturnError {
+            completionHandler(nil)
+        } else {
+            completionHandler(MockNetworkFakeData.fakeTeamDetailsResponse)
+        }
+    }
+}
+
+extension MockNetworkFakeData {
+    func fetchTeamDataFromAPI(url: String, param: Parameters, completionHandler: @escaping (MyResponse<Team>?) -> Void) {
+        if shouldReturnError {
+            completionHandler(nil)
+        } else {
+            completionHandler(MockNetworkFakeData.fakeAllTeamsResponse)
+        }
+    }
+}
+
+extension MockNetworkFakeData {
+    func fetchUpComingEventsDataFromAPI(url: String, param: Parameters, completionHandler: @escaping (MyResponse<Event>?) -> Void) {
+        if shouldReturnError {
+            completionHandler(nil)
+        } else {
+            completionHandler(MockNetworkFakeData.fakeUpComingEventResponse)
+        }
+    }
+}
+
+extension MockNetworkFakeData {
+    func fetchLatestEventsDataFromAPI(url: String, param: Parameters, completionHandler: @escaping (MyResponse<Event>?) -> Void) {
+        if shouldReturnError {
+            completionHandler(nil)
+        } else {
+            completionHandler(MockNetworkFakeData.fakeLatestEventResponse)
+        }
+    }
+}
--- SportsApplicationTests/Mocks/TestNetworkMockData.swift
+++ SportsApplicationTests/Mocks/TestNetworkMockData.swift
@@ -23,6 +23,7 @@ class TestNetworkMockData: XCTestCase {
         mockObj = nil
     }
 
+    
     func testFetchDataFromAPI() {
         let allLeaguesURL = "https://apiv2.allsportsapi.com/football/"
         let parameters = ["met": "Leagues", "APIkey": Constants.API_KEY]
@@ -40,19 +41,51 @@ class TestNetworkMockData: XCTestCase {
     }
 
     func testFetchLeagueDataFromAPI_WithoutError() {
-        // Arrange
+     
         let mockNetworkData = MockNetworkFakeData(shouldReturnError: false)
 
-        // Act
+      
         var fetchedLeagueResponse: MyResponse<League>?
         mockNetworkData.fetchLeagueDataFromAPI(url: "", param: [:]) { response in
             fetchedLeagueResponse = response
         }
 
-        // Assert
+   
         XCTAssertNotNil(fetchedLeagueResponse)
         XCTAssertEqual(fetchedLeagueResponse?.success, 1)
         XCTAssertEqual(fetchedLeagueResponse?.result?.count, 1)
         XCTAssertEqual(fetchedLeagueResponse?.result?.first?.league_name, "Ahly")
     }
+    
+    
+    func testFetchTeamDetailsDataFromAPI() {
+
+        let mockNetworkFakeData = MockNetworkFakeData(shouldReturnError: false)
+        
+     
+        mockNetworkFakeData.fetchTeamDetailsDataFromAPI(url: "", param: [:]) { response in
+   
+            XCTAssertNotNil(response)
+            XCTAssertEqual(response?.success, 1)
+            XCTAssertEqual(response?.result?.count, 1)
+            XCTAssertEqual(response?.result?.first?.team_key, 1)
+            XCTAssertEqual(response?.result?.first?.team_name, "name")
+            XCTAssertEqual(response?.result?.first?.team_logo, "logo")
+            XCTAssertEqual(response?.result?.first?.players?.count, 1)
+            XCTAssertEqual(response?.result?.first?.players?.first?.player_image, "player_image_url")
+            XCTAssertEqual(response?.result?.first?.players?.first?.player_name, "John Doe")
+            XCTAssertEqual(response?.result?.first?.coaches?.count, 1)
+            XCTAssertEqual(response?.result?.first?.coaches?.first?.coach_name, "Coach Smith")
+        }
+    }
+
+    func testFetchTeamDetailsDataFromAPIWithError() {
+        let mockNetworkFakeData = MockNetworkFakeData(shouldReturnError: true)
+        mockNetworkFakeData.fetchTeamDetailsDataFromAPI(url: "", param: [:]) { response in
+            XCTAssertNil(response)
+        }
+    }
+    
+    
+    
 }
